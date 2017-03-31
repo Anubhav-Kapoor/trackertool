2534a71b9a1b06e97793329d882155cede0c8109
unknown <SinghB3@01hw1016483.na.corp.owenscorning.com>
2017-03-31 16:28:08 +0530

    New Changes in Database And Functions

--- ProjectTracker.BLL/UserDetailsBLL.cs
+++ ProjectTracker.BLL/UserDetailsBLL.cs
@@ -108,5 +108,84 @@ namespace ProjectTracker.BLL
 
             return value;
         }
+
+         //InsertFunction[Task Details]
+        public int InsertTaskDetailsBLL(String taskDesc, DateTime createdDate, DateTime expiryDate, String createdBy, String assignedTo, String Status)
+        {
+            try
+            {
+                result = userDEL.InsertTaskDetailsDEL(taskDesc, createdDate, expiryDate, createdBy, assignedTo, Status);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            return result;
+        }
+
+
+        //ViewFunction[Task Details]
+        public DataTable ViewTaskDetailsBLL(int TaskId)
+        {
+            DataTable dt = null;
+            try
+            {
+                dt = userDEL.ViewTaskDetailsDEL(TaskId);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+            return dt;
+        }
+
+         //DeleteFunction[Task Details]
+        public int DeleteTaskDetailsBLL(int TaskId)
+        {
+            try
+            {
+                result = userDEL.DeleteTaskDetailsDEL(TaskId);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            return result;
+        }
+        
+        //UpdateFunction[Task Details]
+        public int UpdateUserDetailsBLL(int taskId, String taskDesc, DateTime createdDate, DateTime expiryDate, String createdBy, String assignedTo, String Status)
+        {
+            try
+            {
+
+                result = userDEL.UpdateUserDetailsDEL(taskId, taskDesc, createdDate, expiryDate, createdBy, assignedTo, Status);
+
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            return result;
+        }
+
     }
 }
